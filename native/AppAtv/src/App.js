c8a7ea4327c0d3d4e3025efc1184c9fa183ab94f
souzayasmin <ysmin.souza@hotmail.com>
2025-02-26 12:57:21 -0300

    atividade do dia 24/02

--- native/AppAtv/src/App.js
+++ native/AppAtv/src/App.js
@@ -1,51 +1,16 @@
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 import Cadastro from "./screens/Cadastro";
+import Login from "./screens/Cadastro";
+import { NavigationConteiner } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Cadastro/>
-    </View>
+    <NavigationConteiner>
+      <Stack.Navigator>
+        <Stack.Screen />
+      </Stack.Navigator>
+    </NavigationConteiner>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  box1: {
-    width: 100,
-    height: 100,
-    backgroundColor: "red",
-  },
-  box2: {
-    width: 100,
-    height: 100,
-    backgroundColor: "yellow",
-  },
-  box3: {
-    width: 100,
-    height: 100,
-    backgroundColor: "green",
-  },
-  box4: {
-    width: 100,
-    height: 100,
-    backgroundColor: "blue",
-  },
-  row: {
-    flexDirection: "row",
-  },
-  text: {
-    fontSize: 25,
-    fontWeight: "bold",
-  },
-  input: {
-    borderColor: "black",
-    borderWidth: 1,
-    width: 200,
-    marginVertical: 20,
-  },
-});
